5d33e7ecb8caccc6f16890b87214dee55ebcddaa
erpranto <erpranto55@gmail.com>
2026-02-23 19:46:49 +0600

    Header and button added

---
+++ script.js
@@ -0,0 +1,5 @@
+let total = document.getElementById("total");
+let interviewCount = document.getElementById("interview-count");
+let rejectCount = document.getElementById("rejected-count");
+
+let allFilterButton=document.getElementById('')

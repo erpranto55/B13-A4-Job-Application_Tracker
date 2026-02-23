d4f4c16221f1b0c7eff3fb97d4ed7d1c0c1d670a
erpranto <erpranto55@gmail.com>
2026-02-24 00:16:20 +0600

    Cards added but not coppied

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+let interviewList = [];
+let rejectedList = [];
+
 let total = document.getElementById("total");
 let interviewCount = document.getElementById("interview-count");
 let rejectCount = document.getElementById("rejected-count");
 
-let allFilterButton=document.getElementById('')
+let allFilterBtn = document.getElementById("all-btn");
+let interviewFilterBtn = document.getElementById("interview-btn");
+let rejectedFilterBtn = document.getElementById("rejected-btn");
+
+let allCardSection = document.getElementById("allCards");
+let mainContainer = document.querySelector("main");
+
+function calculateCount() {
+  total.innerText = allCardSection.children.length;
+  interviewCount.innerText = interviewList.length;
+  rejectCount.innerText = rejectedList.length;
+}
+
+calculateCount();
+
+
